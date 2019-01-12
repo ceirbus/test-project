c0fe980ec7892f5b1ca00e57316bf5b2a048ab91
Will Faircloth <ceirbus@gmail.com>
2019-01-11 20:54:34 -0500

    add scroll methods and change detection

--- src/app/content-scroll/content-scroll.component.ts
+++ src/app/content-scroll/content-scroll.component.ts
@@ -1,53 +1,112 @@
-import { AfterContentInit, Component, OnInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { AfterContentInit, Component, ElementRef, ViewChild, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'content-scroll',
   templateUrl: './content-scroll.component.html',
   styleUrls: ['./content-scroll.component.css']
 })
-export class ContentScrollComponent implements OnInit {
+export class ContentScrollComponent implements AfterContentInit, OnDestroy {
 
   public hasVerticalScroll: boolean = false;
   public hasHorizontalScroll: boolean = false;
 
+  public observer: MutationObserver;
+
   @ViewChild('content') content: ElementRef;
   @ViewChild('wrapper') wrapper: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
-    
+  constructor() {
+    this.observer = new MutationObserver((mutations) => {
+      this.initScroll();
+    });
   }
 
-  ngAfterContentInit() {
+  public ngAfterContentInit() {
+    this.initScroll();
+    this.initObserver();
+  }
+
+  public ngOnDestroy() {
+    this.observer.disconnect();
+  }
+
+  /**
+   * Method to initialize the scroll components when the content is updated
+   */
+  public initScroll(): void {
     this.hasVerticalScroll = this.content.nativeElement.scrollWidth > this.wrapper.nativeElement.clientWidth;
     this.hasHorizontalScroll = this.content.nativeElement.scrollHeight > this.wrapper.nativeElement.clientHeight;
-
-    console.log(this.hasVerticalScroll);
-    console.log(this.hasHorizontalScroll);
   }
 
-  ngOnDestroy() {
+  /**
+   * Method to initialize the MutationObserver that detects changes to the content and updates the controls
+   */
+  public initObserver(): void {
+    const config: MutationObserverInit = {
+      attributes: true,
+      childList: true,
+      characterData: true
+    }
 
+    try {
+      this.observer.observe(this.content.nativeElement, config);
+    } catch(error) {
+      console.log("Error creating observer: ", error);
+    }
   }
 
-  public scrollTopEdge() {
-    console.log(this.content.nativeElement.scrollTop);
+  /**
+   * Method to scroll all the way to the top
+   */
+  public scrollTopEdge(): void {
+    this.wrapper.nativeElement.scrollTop = 0;
   }
 
-  public scrollBottomEdge() {
-    this.content.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
+  /**
+   * Method to scroll top a bit
+   */
+  public scrollTop(): void {
+    this.wrapper.nativeElement.scrollTop -= this.wrapper.nativeElement.clientHeight;
   }
 
-  public scrollLeftEdge() {
-    this.content.nativeElement.scrollLeft = 0;
+  /**
+   * Method to scroll all the way to the bottom
+   */
+  public scrollBottomEdge(): void {
+    this.wrapper.nativeElement.scrollTop = this.content.nativeElement.scrollHeight;
   }
 
-  public scrollRightEdge() {
-    this.content.nativeElement.scrollLeft = this.content.nativeElement.scrollWidth;
+  /**
+   * Method to scroll to the bottom a bit
+   */
+  public scrollBottom(): void {
+    this.wrapper.nativeElement.scrollTop += this.wrapper.nativeElement.clientHeight;
   }
-}
 
-export class ContentScrollElement {
-  
+  /**
+   * Method to scroll all the way left
+   */
+  public scrollLeftEdge(): void {
+    this.wrapper.nativeElement.scrollLeft = 0;
+  }
+
+  /**
+   * Method to scroll to the left a bit
+   */
+  public scrollLeft(): void {
+    this.wrapper.nativeElement.scrollLeft -= this.wrapper.nativeElement.clientWidth;
+  }
+  /**
+   * Method to scroll all the way right
+   */
+  public scrollRightEdge(): void {
+    this.wrapper.nativeElement.scrollLeft = this.content.nativeElement.scrollWidth;
+  }
+
+  /**
+   * Method to scroll to the right a bit
+   */
+  public scrollRight(): void {
+    this.wrapper.nativeElement.scrollLeft += this.wrapper.nativeElement.clientWidth;
+  }
 }
