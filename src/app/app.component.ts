43d6cfdbf3af942e9135812b9d5e4c23223a6f46
Will Faircloth <>
2019-02-21 21:11:09 -0500

    changeContent rework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,29 @@ import { ContentScrollComponent } from './content-scroll/content-scroll.componen
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public onOff: boolean = true;
+  public iterator: number = 0;
+  public filler: string;
   @ViewChild('contentScroll') contentScroll: ContentScrollComponent;
 
   public changeContent() {
-    this.contentScroll.content.nativeElement.innerHTML = this.onOff ? "Lorem Ipsum" :
-    `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
-    this.onOff = !this.onOff;
+    switch(this.iterator) {
+      case 0: {
+        this.iterator += 1;
+        this.filler = 'Lorem Ipsum';
+        break;
+      }
+      case 1: {
+         this.iterator += 1;
+         this.filler = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+         break;
+      }
+      default: {
+         this.iterator = 0;
+         this.filler = '<div style="height: 5000px; width: 5000px; background: red;"></div>';
+         break;
+      }
+   } 
+   this.contentScroll.content.nativeElement.innerHTML = this.filler;
   }
 }
 
